831fc80a64f34512248afdcd9b4b81408a42d063
RitikKumawat <rtkkmwt@gmail.com>
2024-02-10 19:04:15 +0530

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
+    origin:'*',
     credentials:true,
 }))
 app.use(express.static(path.join(__dirname,"../../frontend/dist")));
